6672be94899e1d9f4adfed454b5f5eb6466b1941
Sophia Okosodo <sophieokosodo@gmail.com>
2022-08-01 14:58:36 +0100

    second method

--- index.js
+++ index.js
@@ -60,11 +60,32 @@ if(vowelCount > consonantCount){
 
 
 //second largest and second smallest
-var numberArray = [2, 6, 10, 3, 7, 9, 1]
+var numberArray = [2, 6, 10, 3, 7, 9, 1, 12]
 var solvedArray = []
+
 numberArray.sort(function(a, b){
     return a-b
 })
 solvedArray.push(numberArray[1])
 solvedArray.push(numberArray[numberArray.length-2])
 console.log(solvedArray)
+
+
+//second method for last question
+var newNumberArray = []
+var solved = []
+for(i = 1; i < numberArray.length; i++){
+    for(j = 0; j< numberArray.length; j++){
+        if(numberArray[j] < numberArray[i]){
+            var number = numberArray[j]
+        }
+    }
+    newNumberArray.push(number)
+}
+newNumberArray.push(numberArray[numberArray.length - 1])
+// console.log(newNumberArray)
+solved.push(newNumberArray[1])
+solved.push(newNumberArray[newNumberArray.length-2])
+
+console.log(solved)
+
